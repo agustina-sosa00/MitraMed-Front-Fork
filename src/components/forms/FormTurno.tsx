637a9e14518f99dd12e766862c0f016fc74c58f8
NovaGestionSoftware <novagestionsoftware@gmail.com>
2024-11-12 18:13:02 -0300

    Avance en turnero, config perfil, link a pagina nova, cambio de views

--- src/components/forms/FormTurno.tsx
+++ src/components/forms/FormTurno.tsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { obtenerDoctores, obtenerEspecialidades } from '../../services/TurnosService';
 import { Doctor, Especialidad } from '../../types/index';
+import { FaCalendarAlt } from 'react-icons/fa';
+import { registerLocale } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from 'react-datepicker';
+import Select from 'react-select';
+import { es } from 'date-fns/locale/es';
 // import { getDateFromIddia } from '../../utils/index';
-
-// Estado inicial con el día de mañana
-const today = new Date();
-const tomorrow = new Date(today);
-tomorrow.setDate(today.getDate() + 1);
 
 export default function FormTurno() {
   const [idEspecialidad, setIdespecialidad] = useState<string>('');
-  const [idDoctor, setiDdoctor] = useState<string>('');
+  const [idDoctor, setIddoctor] = useState<string>('');
+  const [fecha, setFecha] = useState<Date | null>(null);
+
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  const fechaDeshabilitada = !idEspecialidad || !idDoctor;
+
+  // Controlar cuando se vacíen idEspecialidad o idDoctor
+  useEffect(() => {
+    if (!idEspecialidad || !idDoctor) {
+      setFecha(null); // Limpiar la fecha si no hay especialidad ni doctor
+    }
+  }, [idEspecialidad, idDoctor]);
+
+  registerLocale('es', es); // Registra el idioma español
 
   console.log(idDoctor);
   // const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 
-  const { data: especialidades } = useQuery<Especialidad[], Error>({
+  const { data: especialidades, isLoading: loadingEspecialidades } = useQuery<
+    Especialidad[],
+    Error
+  >({
     queryKey: ['especialidades'],
     queryFn: obtenerEspecialidades,
   });
@@ -46,60 +66,108 @@ export default function FormTurno() {
   //   console.log(index);
   // };
 
+  // Estructura las opciones para los select
+  const especialidadOptions = especialidades?.map((esp) => ({
+    value: esp.idespecialidad.toString(),
+    label: esp.nombre,
+  }));
+
+  const doctorOptions = doctores?.map((doc) => ({
+    value: doc.iddoctor.toString(),
+    label: `${doc.apellido.toUpperCase()}, ${doc.nombre}`,
+  }));
+
   return (
-    <>
-      <div className="flex flex-col w-full gap-3 my-5">
-        <div className="flex items-center justify-between gap-2 max-w-xl">
+    <div className="flex justify-center mr-32">
+      <div className="flex flex-col items-start gap-3 my-5 ">
+        <div className="flex items-center justify-between gap-2 max-w-xl ">
           <label
             htmlFor="especialidad"
-            className="w-44 text-lg text-gray-800 font-semibold text-right"
+            className="w-44 text-lg text-gray-200 font-semibold text-right "
           >
             Especialidad:
           </label>
-          <select
-            name="especialidad"
-            id="especialidad"
-            className="p-0.5 bg-white flex-1 ml-2 rounded text-base"
-            onChange={(e) => setIdespecialidad(e.target.value)}
-          >
-            <option value="">-- Seleccione --</option>
-            {especialidades?.map((especialidades) => (
-              <option key={especialidades.idespecialidad} value={especialidades.idespecialidad}>
-                {especialidades.nombre}
-              </option>
-            ))}
-          </select>
+
+          <Select
+            options={especialidadOptions}
+            onChange={(e) => {
+              setIdespecialidad(e?.value || '');
+              setIddoctor('');
+            }}
+            value={especialidadOptions?.find((opt) => opt.value === idEspecialidad)}
+            placeholder="Selecciona una especialidad"
+            isClearable
+            isLoading={loadingEspecialidades}
+            loadingMessage={() => 'Cargando especialidades...'}
+            className="w-72 border-0 text-sm placeholder:text-gray-300 ml-2 focus:outline-none focus:ring-0"
+            styles={{
+              control: (provided) => ({ ...provided, boxShadow: 'none' }),
+              placeholder: (provided) => ({
+                ...provided,
+                color: '#A0A0A0', // Gris un poco menos oscuro
+              }),
+            }}
+          />
         </div>
-        <div className="flex items-center justify-between gap-2  max-w-xl">
-          <label
-            htmlFor="especialidad"
-            className="w-44 text-lg text-gray-800 font-semibold text-right"
-          >
+
+        <div className="flex items-center justify-between gap-2 max-w-xl">
+          <label htmlFor="doctor" className="w-44 text-lg text-gray-200 font-semibold text-right">
             Profesional:
           </label>
-          <select
-            name="especialidad"
-            id="especialidad"
-            className="p-0.5 bg-white flex-1 ml-2 rounded text-base"
-            onChange={(e) => setiDdoctor(e.target.value)}
-          >
-            <option value="">-- Seleccione --</option>
-            {loadingDoctores ? (
-              <option disabled>Cargando profesionales...</option> // Mensaje mientras se carga
-            ) : (
-              doctores?.map((doctor) => (
-                <option key={doctor.iddoctor} value={doctor.iddoctor}>
-                  {doctor.apellido.toUpperCase()}, {doctor.nombre}
-                </option>
-              ))
-            )}
-          </select>
+          <Select
+            options={doctorOptions}
+            onChange={(e) => setIddoctor(e?.value || '')}
+            value={idDoctor ? doctorOptions?.find((opt) => opt.value === idDoctor) : null}
+            placeholder="Selecciona un doctor"
+            noOptionsMessage={() => 'Sin opciones'}
+            isClearable
+            isLoading={loadingDoctores}
+            loadingMessage={() => 'Cargando doctores...'}
+            className="w-72 border-0 text-sm  ml-2 focus:outline-none focus:ring-0"
+            styles={{
+              control: (provided) => ({ ...provided, boxShadow: 'none' }),
+              placeholder: (provided) => ({
+                ...provided,
+                color: '#A0A0A0', // Gris un poco menos oscuro
+              }),
+            }}
+          />
         </div>
-        {/* <div className="flex items-center justify-between gap-2  max-w-xl">
-          <label htmlFor="horario" className="w-44 text-lg text-gray-800 font-semibold text-right">
+
+        <div className="flex items-center justify-between gap-2 max-w-xl">
+          <label htmlFor="fecha" className="w-44 text-lg text-gray-200 font-semibold text-right">
+            Fecha:
+          </label>
+
+          <DatePicker
+            selected={fecha} // Fecha seleccionada
+            onChange={(date) => setFecha(date)} // Cambia la fecha
+            minDate={new Date(new Date().setDate(new Date().getDate() + 1))}
+            maxDate={new Date(new Date().setMonth(new Date().getMonth() + 1))}
+            placeholderText="Selecciona una fecha"
+            disabled={fechaDeshabilitada}
+            toggleCalendarOnIconClick
+            locale="es"
+            dateFormat="dd 'de' MMMM 'de' yyyy"
+            showIcon
+            icon={
+              <FaCalendarAlt
+                className={`absolute right-1 top-1/2 transform -translate-y-1/2 text-sm text-gray-500 ${
+                  fechaDeshabilitada ? 'hover:cursor-not-allowed' : 'hover:cursor-pointer'
+                }`}
+              />
+            }
+            className={`w-72 border border-neutral-400 border-opacity-60 bg-white rounded text-sm ml-2 pl-3 ${
+              fechaDeshabilitada ? 'cursor-not-allowed' : 'cursor-pointer'
+            }`}
+          />
+        </div>
+
+        <div className="flex items-center justify-between gap-2  max-w-xl">
+          <label htmlFor="horario" className="w-44 text-lg text-gray-200 font-semibold text-right">
             Horarios:
           </label>
-          <div className="bg-white flex-1 ml-2 rounded text-base border border-gray-300">
+          {/* <div className="bg-white flex-1 ml-2 rounded text-base border border-gray-300">
             <table className="w-full border border-gray-300 text-base bg-white">
               <thead>
                 <tr className="bg-gray-100">
@@ -133,9 +201,9 @@ export default function FormTurno() {
                 )}
               </tbody>
             </table>
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/views/Turnos.tsx
+++ src/views/Turnos.tsx
@@ -1,28 +1,38 @@
+import { Link } from 'react-router-dom';
 import FormTurno from '../components/forms/FormTurno';
 
 export default function Turnos() {
   return (
-    <>
-      <div className="p-6 pb-10 w-full max-w-6xl mx-auto mt-5 bg-gray-200 shadow-xl border border-black border-opacity-30">
-        <div className="">
-          <h2 className="text-3xl font-semibold text-center underline uppercase">Turnos online</h2>
-          <p className="mt-8 mb-4 text-lg text-center">
+    <div className="flex justify-center relative">
+      <div className="absolute mt-5 left-5 top-1/3">
+        <Link
+          to="/inicio"
+          className="py-1 px-6 text-sm font-semibold bg-gray-700 hover:bg-gray-800 text-white transition duration-200"
+        >
+          Volver
+        </Link>
+      </div>
+      <div className="p-6 pb-10 w-full max-w-4xl h-full  my-5 bg-blue-800 shadow-xl border border-black border-opacity-20 rounded-lg">
+        {/* Botón de enlace para volver al inicio */}
+        <div className="text-center">
+          <h2 className="text-3xl font-bold text-gray-200 underline uppercase mb-4">
+            Turnos Online
+          </h2>
+          <p className="mt-6 mb-8 text-lg text-gray-200">
             Selecciona especialidad y profesional para buscar disponibilidad de turnos
           </p>
         </div>
-        <div className="">
-          <form className="flex flex-col">
-            <FormTurno />
-            <div className="flex justify-center">
-              <input
-                type="submit"
-                value="Solicitar"
-                className="p-2 mt-8 max-w-xl w-full text-white text-lg bg-green-600 hover:bg-green-700 uppercase font-semibold cursor-pointer transition"
-              />
-            </div>
-          </form>
-        </div>
+        <form className="flex flex-col items-center">
+          <FormTurno />
+          <div className="flex justify-center w-full">
+            <input
+              type="submit"
+              value="Solicitar"
+              className="p-3 mt-8 max-w-lg w-full text-lg bg-green-600 hover:bg-green-700 text-white uppercase font-semibold rounded-lg cursor-pointer transition duration-200"
+            />
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
