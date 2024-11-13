6899b7dcedc0a6f0d214a0fb3fd696391523508a
NovaGestionSoftware <novagestionsoftware@gmail.com>
2024-11-13 10:17:28 -0300

    Borde logo y arreglo portal

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center gap-3">
-      <Link to="/" className="mx-5 flex gap-3">
+    <div className="flex flex-col md:flex-row justify-center items-center gap-3 p-2 border-[3px] border-slate-400 rounded-lg">
+      <Link to="/" className="mx-3 flex gap-3">
         <img src="/logos/mitra-med-logo-no-bg.png" alt="logo" className="max-w-20 max-h-20" />
       </Link>
       <div className="hidden md:flex items-center">
