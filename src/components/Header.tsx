c6cef0c6a749787e5b4dd199d33cd0bb633b2652
NovaGestionSoftware <novagestionsoftware@gmail.com>
2024-11-21 18:25:59 -0300

    Logo dirrecionado segun Login

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import Logo from './Logo';
+import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import Nav from './Nav';
 
@@ -12,8 +12,17 @@ export default function Header() {
       } `}
     >
       <div className="flex justify-between items-center mx-5 md:mx-10">
-        <div className="">
-          <Logo />
+        <div className="flex flex-col md:flex-row justify-center items-center gap-3 p-2 border-[3px] border-slate-400 rounded-lg">
+          <Link to={`${isLoggedIn ? '/inicio' : '/'}`} className="mx-3 flex gap-3">
+            <img src="/logos/mitra-med-logo-no-bg.png" alt="logo" className="max-w-20 max-h-20" />
+          </Link>
+          <div className="hidden md:flex items-center">
+            <img
+              src="/logos/mitra-med-letras_no-bg1.png"
+              alt="logo-letras"
+              className="max-w-44 max-h-44"
+            />
+          </div>
         </div>
 
         {!isLoggedIn && (
