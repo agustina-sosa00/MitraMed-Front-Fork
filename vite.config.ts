a50bdc8a9dc722298fca7bb518dc766246d889f0
NovaGestionSoftware <novagestionsoftware@gmail.com>
2024-12-26 12:39:33 -0300

    Botton flotante WhatsApp

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/layouts': path.resolve(__dirname, 'src/layouts'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+      '@/services': path.resolve(__dirname, 'src/services'),
+      '@/types': path.resolve(__dirname, 'src/types'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
+      '@/views': path.resolve(__dirname, 'src/views'),
+    },
+  },
 });
