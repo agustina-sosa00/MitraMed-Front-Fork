479cf05816df56406901d598295d76693f285c18
nova <novagestionsoftware@gmail.com>
2025-06-03 11:24:09 -0300

    mejora del home responsive y funcional

--- src/components/ui/CarrouselPortal.tsx
+++ src/components/ui/CarrouselPortal.tsx
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { arrayInfoCarousel } from "../../mock/arrayInfoCarousel";
 
-export default function CarrouselPortal() {
+interface IProp {
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function CarrouselPortal({ state, setState }: IProp) {
   const [imageLoaded, setImageLoaded] = useState(false);
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -24,115 +30,53 @@ export default function CarrouselPortal() {
     },
   };
 
+  const handleOpenDrawer = () => {
+    setState(!state);
+  };
   return (
-    <div className="relative h-full lg:h-[480px] xl:h-[600px] w-full overflow-hidden mb-5">
+    <div className="relative h-full lg:h-[90vh] w-full overflow-hidden mb-5">
       <Slider {...sliderSettings}>
-        <div className="relative">
-          <img
-            src="/med/doc-1.webp"
-            alt="Doctor"
-            className={`w-full h-full object-cover ${
-              imageLoaded ? "" : "opacity-0"
-            }`}
-            onLoad={handleImageLoad}
-            style={{ pointerEvents: "none" }}
-          />
-          <div
-            className={`absolute top-1/2 mx:top-1/3 text-center mb-5 ml-5 mx:mt-5 transition-opacity duration-1000 ${
-              imageLoaded && currentSlide === 0 ? "opacity-100" : "opacity-0"
-            } sm:block hidden`}
-          >
-            <div className="flex flex-col items-start max-w-sm px-2 py-4 mt-6 ml-3 bg-black rounded-lg bg-opacity-60 mx:max-w-md xl:max-w-xl mx:px-6 mx:py-6 xl:p-10">
-              <h2 className="mb-3 text-xl font-semibold text-white mx:text-2xl xl:text-3xl">
-                Simplifica tu acceso a la {""}{" "}
-                <span className="text-blue-400">salud</span>!
-              </h2>
-              <p className="text-xs text-white mx:text-base xl:text-lg text-start mx:mb-4">
-                Agenda tus consultas médicas en pocos pasos y sin
-                complicaciones.
-              </p>
-              <div className="hidden p-2 text-xs text-white bg-blue-600 rounded-lg mx:block mx:text-base xl:text-lg xl:p-3 text-start">
-                <p>
-                  ¡Crea tu {""}
-                  <span className="underline"> cuenta</span> y conoce todos
-                  nuestros servicios!
+        {arrayInfoCarousel.map((item, index) => (
+          <div className="relative" key={index}>
+            <img
+              src={item.img}
+              alt="MitraMed"
+              className={` w-full  object-cover   ${
+                imageLoaded ? "" : "opacity-0"
+              }`}
+              onLoad={handleImageLoad}
+              style={{ pointerEvents: "none" }}
+            />
+
+            <div
+              className={`absolute top-0  bg-[linear-gradient(to_top_right,white_0%,rgba(255,255,255,0.8)_40%,rgba(255,255,255,0.5)_50%,rgba(255,255,255,0.2)_75%,transparent_100%)] w-full h-full `}
+            >
+              <div
+                className={`absolute transition-opacity duration-1000 w-full md:w-1/2 lg:w-3/6 bottom-5 top-32 md:top-64 xl:top-80 2xl:top-96 left-5 md:left-12   ${
+                  imageLoaded && currentSlide === index
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
+              >
+                <h1 className="mb-3  w-[90%] font-semibold text-[var(--color-blue)] text-xl md:text-3xl lg:text-5xl">
+                  {item.title}
+                  <span className="text-[var(--color-green-hover)]">
+                    {item.span}
+                  </span>
+                </h1>
+                <p className=" text-[var(--color-blue)] text-sm w-2/3 md:text-base lg:text-lg xl:text-xl text-start mb-2 md:mb-4">
+                  {item.text}
                 </p>
+                <button
+                  className=" p-2 text-sm md:text-base text-white bg-[var(--color-green)] hover:bg-[var(--color-green-hover)] cursor-pointer rounded-lg mx:block lg:text-lg xl:text-xl xl:p-3 text-start"
+                  onClick={handleOpenDrawer}
+                >
+                  <p>{item.textoImportant}</p>
+                </button>
               </div>
             </div>
           </div>
-        </div>
-
-        {/* Agrega el mismo comportamiento para las otras imágenes */}
-        <div className="relative">
-          <img
-            src="/med/doc-7.webp"
-            alt="Doctor2"
-            className={`w-full h-full object-cover ${
-              imageLoaded ? "" : "opacity-0"
-            }`}
-            onLoad={handleImageLoad}
-            style={{ pointerEvents: "none" }}
-          />
-
-          <div
-            className={`absolute top-1/2 mx:top-1/3 text-center mb-5 ml-5 mx:mt-5 transition-opacity duration-1000 ${
-              imageLoaded && currentSlide === 1 ? "opacity-100" : "opacity-0"
-            } sm:block hidden`}
-          >
-            <div className="flex flex-col items-start max-w-sm px-2 py-4 mt-6 ml-3 bg-black rounded-lg bg-opacity-60 mx:max-w-md xl:max-w-lg mx:px-6 mx:py-6 xl:p-10">
-              <h2 className="mb-3 text-xl font-semibold text-white mx:text-2xl xl:text-3xl">
-                Atención {""}{" "}
-                <span className="text-blue-400"> personalizada</span>
-              </h2>
-              <p className="text-xs text-white mx:text-base xl:text-lg text-start mx:mb-4">
-                Ofrecemos un {""}
-                <span className="underline"> servicio {""}</span> cercano y
-                adaptado a tus necesidades.
-              </p>
-              <div className="hidden p-2 text-xs text-white bg-blue-600 rounded-lg mx:block mx:text-base xl:text-lg text-start">
-                <p>
-                  ¡Comienza tu camino hacia la {""}
-                  <span className="underline"> salud {""}</span> hoy!
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative">
-          <img
-            src="/med/doc-2.jpg"
-            alt="Doctor"
-            className={`w-full h-full object-cover ${
-              imageLoaded ? "" : "opacity-0"
-            }`}
-            onLoad={handleImageLoad}
-            style={{ pointerEvents: "none" }}
-          />
-          <div
-            className={`absolute top-1/2 mx:top-1/3 text-center mb-5 ml-5 mx:mt-5 transition-opacity duration-1000 ${
-              imageLoaded && currentSlide === 2 ? "opacity-100" : "opacity-0"
-            } sm:block hidden`}
-          >
-            <div className="flex flex-col items-start max-w-sm px-2 py-4 mt-6 ml-3 bg-black rounded-lg bg-opacity-60 mx:max-w-md xl:max-w-xl mx:px-6 mx:py-6 xl:p-10">
-              <h2 className="mb-3 text-xl font-semibold text-white mx:text-2xl xl:text-3xl">
-                Cuidado médico a tu {""}{" "}
-                <span className="text-blue-400"> alcance</span>
-              </h2>
-
-              <p className="text-xs text-white mx:text-base xl:text-lg text-start mx:mb-4">
-                Accede a consultas médicas con solo {""}
-                <span className="underline"> iniciar sesión{""}</span>.
-              </p>
-              <div className="hidden p-2 text-xs text-white bg-blue-600 rounded-lg mx:block mx:text-base xl:text-lg text-start">
-                <p>
-                  ¡Haz un seguimiento de tus {""}
-                  <span className="underline"> turnos</span>!
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </Slider>
     </div>
   );
