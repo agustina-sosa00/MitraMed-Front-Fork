ba740cd5a0c70f20229d9307ece9a382650a5ec5
nova <novagestionsoftware@gmail.com>
2025-06-09 19:52:09 -0300

    sidebar funcional y la tabla muestra datos quemados

--- src/components/ui/SideBar.tsx
+++ src/components/ui/SideBar.tsx
@@ -1,31 +1,70 @@
 import React from "react";
+import { FaUserCircle } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 interface IProp {
   logo: string;
   buttons: { name: string; icon: IconType; link: string }[];
 }
 
 export const SideBar: React.FC<IProp> = ({ logo, buttons }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    document.cookie =
+      "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    navigate("/");
+  };
   return (
     <nav className="flex-col justify-between h-screen rounded w-60">
-      <div className="h-full bg-white border-r-2 border-lightGray ">
-        <div className="flex justify-center ">
-          <img src={logo} alt="logo" className=" w-[90%]  " />
+      <section className="flex flex-col justify-between h-full bg-gray-300 ">
+        {/* BOX 1 */}
+        <div className="flex  pl-8 items-center h-[10%] ">
+          {/* <h2 className="px-2 py-5 text-lg font-bold tracking-wider uppercase text-blue">
+            Panel <br /> <span className=""> Profesional </span>
+          </h2> */}
+          <img src={logo} alt="logo" className="" />
         </div>
-        <div className="w-full border border-greenFocus "></div>
-        <div className="flex flex-col w-full gap-3 px-3 py-3">
+        <div className="flex justify-center w-full">
+          <div className="w-[80%] bg-blue h-[1px] "></div>
+        </div>
+        {/* BOX 2 */}
+
+        <div className="flex flex-col  w-full gap-3 pl-5 py-5 h-[65%] ">
           {buttons.map((item) => (
             <Link
               to={item.link}
-              className="flex items-center gap-1 px-2 py-1 text-lg font-medium capitalize rounded cursor-pointer text-blue hover:bg-green hover:text-white"
+              className="flex items-center gap-2 pl-5  py-1 w-[90%] text-lg font-medium text-blue capitalize rounded cursor-pointer hover:bg-green hover:text-white"
             >
-              <item.icon />
+              <item.icon className="" />
               {item.name}
             </Link>
           ))}
         </div>
-      </div>
+        <div className="flex justify-center w-full">
+          <div className="w-[80%] bg-blue h-[1px] "></div>
+        </div>
+        {/* BOX 3 */}
+
+        <div className="flex flex-col  w-full gap-3 pl-10 py-5    h-[20%]  ">
+          <div className="flex items-center justify-start gap-2 text-blue ">
+            <FaUserCircle className="text-xl xl:text-3xl" />{" "}
+            <p>Dr. Juan Lopez</p>
+          </div>
+          <p className="text-sm ">
+            ¿Quieres{" "}
+            <span
+              onClick={handleLogout}
+              className="font-bold cursor-pointer hover:text-green"
+            >
+              cerrar sesión{" "}
+            </span>
+            ?{" "}
+          </p>
+        </div>
+      </section>
     </nav>
   );
 };
---
+++ src/mock/arrayTableProfessional.ts
@@ -0,0 +1,92 @@
+export const tableSchedules = [
+  {
+    id: 1,
+    day: "08/06/2025",
+    hourInit: "18:30",
+    hourFinish: "18:30",
+    name: "Agustina Sosa",
+    state: "presente",
+    obs: "IOMA",
+  },
+  {
+    id: 2,
+    day: "15/06/2025",
+    hourInit: "18:30",
+    hourFinish: "18:30",
+    name: "Leandro Mortarini",
+    state: "ausente",
+    obs: "SWISS",
+  },
+  {
+    id: 3,
+    day: "09/06/2025",
+    hourInit: "18:30",
+    hourFinish: "18:30",
+    name: "Jean Pietro Mortarini",
+    state: "",
+    obs: "",
+  },
+  {
+    id: 2,
+    day: "15/06/2025",
+    hourInit: "18:30",
+    hourFinish: "18:30",
+    name: "Leandro Mortarini",
+    state: "presente",
+    obs: "MEDIFE",
+  },
+  {
+    id: 3,
+    day: "09/06/2025",
+    hourInit: "18:30",
+    hourFinish: "18:30",
+    name: "Jean Pietro Mortarini",
+    state: "",
+    obs: "",
+  },
+];
+
+export const tableColumnData = [
+  {
+    key: "id",
+    label: "ID",
+    minWidth: "40",
+    maxWidth: "40",
+  },
+  {
+    key: "day",
+    label: "DÍA",
+    minWidth: "150",
+    maxWidth: "150",
+  },
+  {
+    key: "hourInit",
+    label: "HORA INICIO",
+    minWidth: "100",
+    maxWidth: "100",
+  },
+  {
+    key: "hourFinish",
+    label: "HORA FIN",
+    minWidth: "100",
+    maxWidth: "100",
+  },
+  {
+    key: "name",
+    label: "NOMBRE Y APELLIDO",
+    minWidth: "400",
+    maxWidth: "400",
+  },
+  {
+    key: "state",
+    label: "ESTADO",
+    minWidth: "100",
+    maxWidth: "100",
+  },
+  {
+    key: "obs",
+    label: "OBRA SOCIAL",
+    minWidth: "100",
+    maxWidth: "100",
+  },
+];
