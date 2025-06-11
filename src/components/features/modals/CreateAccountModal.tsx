aa653d10b1df0c554bc2de8ed680aca61e7f7870
nova <novagestionsoftware@gmail.com>
2025-06-11 15:30:16 -0300

    tercer prueba de los datos del form

--- src/components/features/modals/CreateAccountModal.tsx
+++ src/components/features/modals/CreateAccountModal.tsx
@@ -7,6 +7,7 @@ import { crearCuenta } from "@/services/UserService";
 import { toast } from "react-toastify";
 import RegisterForm from "../forms/RegisterForm";
 import { Modal } from "@/components/ui/Modal";
+import { useEffect } from "react";
 
 export default function CreateAccountModal() {
   const navigate = useNavigate();
