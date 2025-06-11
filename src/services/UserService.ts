0c6280a166e6a8bf7c7199ab863d79708c4270fc
nova <novagestionsoftware@gmail.com>
2025-06-11 15:06:51 -0300

    primera prueba de completar los datos de usuario

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,26 +1,27 @@
-import { isAxiosError } from 'axios';
-import { Account, NewAccount, UserGoogle } from '../types';
-import apiNoAuth from '@/lib/axiosNoAuth';
+import { isAxiosError } from "axios";
+import { Account, NewAccount, UserGoogle } from "../types";
+import apiNoAuth from "@/lib/axiosNoAuth";
 
 export async function crearCuenta(formData: NewAccount) {
   try {
     const { confirmPassword, ...dataToSend } = formData;
 
-    const { data } = await apiNoAuth.post('/auth/registrar', dataToSend);
+    const { data } = await apiNoAuth.post("/auth/registrar", dataToSend);
+    console.log("data del registro", data);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
 
 export async function olvidePassword(email: { email: string }) {
   try {
-    const { data } = await apiNoAuth.post('/auth/olvide_password', email);
+    const { data } = await apiNoAuth.post("/auth/olvide_password", email);
     // console.log(data);
     return data;
   } catch (error) {
@@ -28,14 +29,14 @@ export async function olvidePassword(email: { email: string }) {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
 
 export async function nuevoToken(email: { email: string }) {
   try {
-    const { data } = await apiNoAuth.post('/auth/reenviar_tokenconfirm', email);
+    const { data } = await apiNoAuth.post("/auth/reenviar_tokenconfirm", email);
     // console.log(data);
     return data;
   } catch (error) {
@@ -43,14 +44,14 @@ export async function nuevoToken(email: { email: string }) {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
 
 export async function iniciarSesion(formData: Account) {
   try {
-    const { data } = await apiNoAuth.post('/auth/iniciar_sesion', formData);
+    const { data } = await apiNoAuth.post("/auth/iniciar_sesion", formData);
     // console.log(data);
 
     return data;
@@ -59,14 +60,14 @@ export async function iniciarSesion(formData: Account) {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
 
 export async function googleAuth(googleData: UserGoogle) {
   try {
-    const { data } = await apiNoAuth.post('/auth/google_auth', googleData);
+    const { data } = await apiNoAuth.post("/auth/google_auth", googleData);
     // console.log(data);
 
     return data;
@@ -75,14 +76,14 @@ export async function googleAuth(googleData: UserGoogle) {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
 
 export async function whatsAppNum(): Promise<{ numero: string }[]> {
   try {
-    const { data } = await apiNoAuth('/auth/obtiene_wspnum');
+    const { data } = await apiNoAuth("/auth/obtiene_wspnum");
     console.log(data);
 
     return data;
@@ -91,7 +92,7 @@ export async function whatsAppNum(): Promise<{ numero: string }[]> {
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
@@ -104,9 +105,12 @@ export async function reestablecerPassword({
   password: string;
 }) {
   try {
-    const { data } = await apiNoAuth.post(`/auth/reestablecer_password?token=${token}`, {
-      password,
-    });
+    const { data } = await apiNoAuth.post(
+      `/auth/reestablecer_password?token=${token}`,
+      {
+        password,
+      }
+    );
     // console.log(data);
     return data;
   } catch (error) {
@@ -114,7 +118,7 @@ export async function reestablecerPassword({
       console.log(error.response.data);
       throw new Error(error.response.data.error);
     } else {
-      throw new Error('Hubo un error...');
+      throw new Error("Hubo un error...");
     }
   }
 }
