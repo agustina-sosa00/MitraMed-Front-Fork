54737381e6411b76d865e22db861644ec09ab3ad
nova <novagestionsoftware@gmail.com>
2025-06-19 19:10:29 -0300

    prueba 4 de modal confirm data user sin el error

--- src/components/features/modals/GoogleAuthModal.tsx
+++ src/components/features/modals/GoogleAuthModal.tsx
@@ -105,6 +105,7 @@ export default function GoogleAuthModal() {
             )
               .then((response) => response.json())
               .then((userData) => {
+                console.log("userdta", userData);
                 const dataSend = {
                   nombre: userData?.names[0]?.givenName || "",
                   apellido: userData?.names[0]?.familyName || "",
