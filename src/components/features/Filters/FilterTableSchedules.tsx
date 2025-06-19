513212c42db4043ff49a003b7aa3ca23aeff77df
nova <novagestionsoftware@gmail.com>
2025-06-19 17:44:13 -0300

    botones para cambiar de fecha en la tabla de turnos + cambios esteticos

--- src/components/features/Filters/FilterTableSchedules.tsx
+++ src/components/features/Filters/FilterTableSchedules.tsx
@@ -1,27 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HiArrowSmLeft } from "react-icons/hi";
+
 interface IProp {
   state: string;
   setState: (ag: string) => void;
+  handle: (num: number) => void;
 }
-export const FilterTableSchedules = ({ state, setState }: IProp) => {
+export const FilterTableSchedules = ({ state, setState, handle }: IProp) => {
+  const [dayColor, setDayColor] = useState<string>("");
+  const [nameDay, setNameDay] = useState("");
+  const newDay = new Date();
+  const day = `${newDay.toISOString().split("T")[0]}`;
+  useEffect(() => {
+    setDayColor(
+      day < state
+        ? "text-red-500"
+        : day == state
+        ? "text-green"
+        : "text-yellow-500"
+    );
+    console.log(state);
+    const selectedDay = new Date(state + "T12:00:00");
+    const options = { weekday: "long" } as const;
+    console.log(selectedDay);
+
+    const name = selectedDay.toLocaleDateString("es-AR", options);
+    console.log(name);
+
+    setNameDay(name);
+  }, [day, state]);
+
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("input", e.target);
     setState && setState(e.target.value);
   };
   return (
     <div className="w-full ">
-      <div className="flex items-center justify-center gap-1 p-2 lg:justify-start w-ful lg:w-1/3 ">
+      <div className="flex items-end justify-center w-full gap-1 p-2 lg:justify-start ">
         <label className="text-sm font-medium text-blue">
-          Filtrar por dia:{" "}
+          Filtrar por dia:
         </label>
-        <input
-          required
-          placeholder=""
-          type="date"
-          className="px-2 py-1 text-sm border border-gray-300 rounded bg-lightGray "
-          name="day"
-          value={state}
-          onChange={handleOnChange}
-        />
+        <button
+          onClick={() => handle(-1)}
+          className="p-1 transition-all duration-200 border border-gray-300 rounded text-blue bg-lightGray hover:bg-gray-300"
+        >
+          <HiArrowSmLeft className="text-2xl lg:text-3xl" />
+        </button>
+        <div className="flex flex-col items-center">
+          <p className={`capitalize lg:text-lg font-bold ${dayColor}`}>
+            <span className="text-sm font-medium text-blue">Día: </span>{" "}
+            {nameDay}
+          </p>
+          <input
+            required
+            placeholder=""
+            type="date"
+            className={`px-2 py-1 lg:text-lg font-bold border border-gray-300 rounded  bg-lightGray ${dayColor}`}
+            name="day"
+            value={state}
+            onChange={handleOnChange}
+          />
+        </div>
+
+        <button
+          onClick={() => handle(1)}
+          className="p-1 transition-all duration-200 border border-gray-300 rounded text-blue bg-lightGray hover:bg-gray-300"
+        >
+          {" "}
+          <HiArrowSmLeft className="text-2xl rotate-180 lg:text-3xl " />
+        </button>
       </div>
     </div>
   );
--- src/components/features/PanelProfessional/TablaMobile.tsx
+++ src/components/features/PanelProfessional/TablaMobile.tsx
@@ -8,15 +8,17 @@ export const TablaMobile: React.FC<TablaMobileProps> = ({ data }) => {
   return (
     <div className="relative max-w-[600px] !rounded overflow-x-auto">
       <table className="w-full text-left text-gray-500 ">
-        <thead className="text-white uppercase bg-blue">
+        <thead className="sticky top-0 text-white uppercase  bg-blue">
           <tr className="">
-            <th className="px-2 py-1 text-xs">ID</th>
-            <th className="px-4 py-3 text-xs">Día</th>
-            <th className="px-4 py-3 text-xs">Hora Inicio</th>
-            <th className="px-4 py-3 text-xs">Hora Fin</th>
-            <th className="px-4 py-3 text-xs">Nombre y Apellido</th>
-            <th className="px-4 py-3 text-xs">Estado</th>
-            <th className="px-4 py-3 text-xs">Obra Social</th>
+            <th className="px-2 py-1 text-xs whitespace-nowrap">ID</th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">Día</th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">Hora Inicio</th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">Hora Fin</th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">
+              Nombre y Apellido
+            </th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">Estado</th>
+            <th className="px-4 py-3 text-xs whitespace-nowrap">Obra Social</th>
           </tr>
         </thead>
         <tbody>
@@ -25,7 +27,7 @@ export const TablaMobile: React.FC<TablaMobileProps> = ({ data }) => {
               key={`${item.id}-${item.day}`}
               className="bg-[#f1f1f1] border-b text-blue border-gray-200 hover:bg-gray-50 odd:bg-white even:bg-[#f1f1f1]"
             >
-              <td className="px-4 py-2 text-xs">{item.id}</td>
+              <td className="px-4 py-2 text-xs text-end">{item.id}</td>
               <td className="px-4 py-2 text-xs">{item.day}</td>
               <td className="px-4 py-2 text-xs">{item.hourInit}</td>
               <td className="px-4 py-2 text-xs">{item.hourFinish}</td>
