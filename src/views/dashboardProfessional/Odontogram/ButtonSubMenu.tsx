8f96b7f3f4da0284b89d9e41d90e17b0a86f6b18
nova <novagestionsoftware@gmail.com>
2025-06-24 18:07:43 -0300

    se marcan las formas en el odontograma

---
+++ src/views/dashboardProfessional/Odontogram/ButtonSubMenu.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+interface ButtonSubMenuProps {
+  width: boolean;
+  actionText: "realizado" | "a realizar";
+  onSelect: (option: {
+    action: "realizado" | "a realizar";
+    tratamiento: string;
+  }) => void;
+}
+const treatmentList = ["Extracción", "Corona", "Restauraciones", "Sellado"];
+
+export const ButtonSubMenu: React.FC<ButtonSubMenuProps> = ({
+  width,
+  actionText,
+  onSelect,
+}) => {
+  return (
+    <div
+      className={`absolute top-0 hidden bg-white border border-gray-300 rounded group-hover:block ${
+        width ? "left-full" : "right-full"
+      }`}
+    >
+      {treatmentList.map((item, index) => (
+        <button
+          key={item}
+          onClick={() =>
+            onSelect({
+              action: actionText,
+              tratamiento: item,
+            })
+          }
+          className={`w-44 px-3 py-2 text-start text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-gray-300 ${
+            index === 0
+              ? "rounded-t"
+              : index === treatmentList.length - 1
+              ? "rounded-b"
+              : ""
+          }`}
+        >
+          {item}
+        </button>
+      ))}
+    </div>
+  );
+};
--- src/views/dashboardProfessional/Odontogram/Tooth.tsx
+++ src/views/dashboardProfessional/Odontogram/Tooth.tsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from "react";
-import { IoIosArrowDown } from "react-icons/io";
+import { MenuTooth } from "./MenuTooth";
+import { FaRegCircle } from "react-icons/fa";
+import { IoMdArrowDropup } from "react-icons/io";
+import { GrClose } from "react-icons/gr";
 
-export const Tooth = ({ toothNumber, isActive, setState }) => {
+interface ToothProps {
+  toothNumber: number;
+  isActive: boolean;
+  setState: (num: number) => void;
+  selectedOption?: {
+    action: "realizado" | "a realizar";
+    tratamiento: string;
+  };
+  onSelectOption: (option: {
+    action: "realizado" | "a realizar";
+    tratamiento: string;
+  }) => void;
+}
+export const Tooth: React.FC<ToothProps> = ({
+  toothNumber,
+  isActive,
+  setState,
+  selectedOption,
+  onSelectOption,
+}) => {
   const [width, setWidth] = useState<boolean>(false);
-  console.log(width);
   const [positionMenu, setPositionMenu] = useState({
     x: 0,
     y: 0,
@@ -30,117 +51,108 @@ export const Tooth = ({ toothNumber, isActive, setState }) => {
 
   useEffect(() => {
     const widthTotal = window.innerWidth;
-    console.log("width total --->", widthTotal);
-    console.log("width click --->", positionMenu.x);
     if (widthTotal - positionMenu.x > 385) {
-      console.log("resta", widthTotal - positionMenu.x);
       setWidth(true);
     } else {
-      console.log("resta", widthTotal - positionMenu.x);
       setWidth(false);
     }
   }, [positionMenu.x]);
 
   return (
-    <div
-      // onClick={() => handleClick(toothNumber)}
-      onContextMenu={(e) => handleContextMenu(e)}
-    >
-      <svg
-        viewBox="0 0 28 33"
-        className="w-8 h-8 lg:h-14 lg:w-14 xl:w-16 xl:h-16 "
-      >
-        <polygon
-          stroke="#a3a3a3" //border
-          fill="#fff" //backgrounds
-          // onClick={() => handleClick("arriba")}
-          className="cursor-pointer "
-          points="1.0136711597442627,1.35626420378685 7.767158031463623,9.155386298894882 21.696229934692383,9.155386298894882 28.449718475341797,1.35626420378685 "
-        />
+    <div className="" onContextMenu={(e) => handleContextMenu(e)}>
+      <div className="relative">
+        {selectedOption && (
+          <div className="absolute w-8 h-8 lg:h-14 lg:w-14 xl:w-16 xl:h-16">
+            {selectedOption.tratamiento === "Corona" && (
+              <FaRegCircle
+                className={`absolute z-40 w-14 h-14 ${
+                  selectedOption.action === "realizado"
+                    ? "text-red-500"
+                    : "text-sky-700"
+                }`}
+              />
+            )}
+            {selectedOption.tratamiento === "Extracción" && (
+              <GrClose
+                className={`absolute  z-40 w-14 h-14 ${
+                  selectedOption.action === "realizado"
+                    ? "text-red-500 "
+                    : "text-sky-700 "
+                }`}
+              />
+            )}
 
-        <polygon
-          stroke="#a3a3a3" //border
-          fill="#fff" //background
-          // onClick={() => handleClick("derecha")}
-          className="cursor-pointer "
-          points="21.445681169629097,9.104242324829102 21.445681169629097,25.189937591552734 28.19916971027851,32.98905944824219 28.41021592915058,0.8176754713058472 "
-        />
+            <div className="w-full h-1/4 "></div>
+            <div className="flex w-full">
+              <div className="w-1/4 h-7 "></div>
+              <div className="flex items-center justify-center w-1/2 h-7">
+                {selectedOption.tratamiento === "Sellado" && (
+                  <IoMdArrowDropup
+                    className={`text-2xl  ${
+                      selectedOption.action === "realizado"
+                        ? "text-red-500 "
+                        : "text-sky-700 "
+                    }`}
+                  />
+                )}
+              </div>
+              <div className="w-1/4 h-7 "></div>
+            </div>
+            <div className="w-full h-1/4 "></div>
+          </div>
+        )}
 
-        <polygon
-          stroke="#a3a3a3" //border
-          fill="#fff" //background
-          // onClick={() => handleClick("abajo")}
-          className="cursor-pointer "
-          points="21.445680618286133,25.29296439886093 28.199169158935547,33.092128217220306 0.7631232142448425,33.092128217220306 7.516610622406006,25.29296439886093 "
-        />
-
-        <polygon
-          stroke="#a3a3a3" //border
-          fill="#fff" //background
-          // onClick={() => handleClick("izquierda")}
-          points="0.7631232291460037,1.3051201105117798 0.7631232291460037,33.232784271240234 7.516610696911812,25.189937591552734 7.516610696911812,25.189937591552734 7.516610696911812,9.104242324829102 "
-          className="cursor-pointer "
-        />
-        <polygon
-          stroke="#a3a3a3" //border
-          fill="#fff" //background
-          // onClick={() => handleClick("centro")}
-          points="7.516610696911812,9.104242324829102 21.445681169629097,9.104242324829102 21.445681169629097,25.189937591552734 7.516610696911812,25.189937591552734 "
-          className="cursor-pointer "
-        />
-      </svg>
-      {isActive && (
-        <div
-          className={`absolute  bg-white shadow shadow-gray-300 h-16  w-52`}
-          style={{ top: positionMenu.y, left: positionMenu.x }}
+        <svg
+          viewBox="0 0 28 33"
+          className="w-8 h-8 lg:h-14 lg:w-14 xl:w-16 xl:h-16 "
         >
-          <div className="relative group">
-            <div className="w-full flex justify-between items-center px-3 border-b border-[#dbdbdb] py-2 cursor-pointer text-blue bg-white text-sm group-hover:text-white group-hover:bg-green transition-all duration-300">
-              Realizado <IoIosArrowDown className="-rotate-90" />
-            </div>
-            <div
-              className={`absolute top-0 hidden bg-white group-hover:block ${
-                width ? "left-full" : "right-full"
-              } `}
-            >
-              <div className="w-44 px-3 py-2 text-sm border-b  cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Extracción
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer  hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Corona
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Restauraciones
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Sellado
-              </div>
-            </div>
-          </div>
-          <div className="relative group">
-            <div className="flex items-center justify-between w-full px-3 py-2 text-sm transition-all duration-300 bg-white cursor-pointer text-blue group-hover:text-white group-hover:bg-green">
-              A realizar <IoIosArrowDown className="-rotate-90" />
-            </div>
-            <div
-              className={`absolute top-0 hidden bg-white group-hover:block ${
-                width ? "left-full" : "right-full"
-              } `}
-            >
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Extracción
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Corona
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Restauraciones
-              </div>
-              <div className="w-full px-3 py-2 text-sm border-b cursor-pointer hover:text-white hover:bg-green transition-all duration-300 border-[#f1f1f1]">
-                Sellado
-              </div>
-            </div>
-          </div>
-        </div>
+          <polygon
+            stroke="#a3a3a3" //border
+            fill="#fff" //backgrounds
+            // onClick={() => handleClick("arriba")}
+            className="cursor-pointer "
+            points="1.0136711597442627,1.35626420378685 7.767158031463623,9.155386298894882 21.696229934692383,9.155386298894882 28.449718475341797,1.35626420378685 "
+          />
+
+          <polygon
+            stroke="#a3a3a3" //border
+            fill="#fff" //background
+            // onClick={() => handleClick("derecha")}
+            className="cursor-pointer "
+            points="21.445681169629097,9.104242324829102 21.445681169629097,25.189937591552734 28.19916971027851,32.98905944824219 28.41021592915058,0.8176754713058472 "
+          />
+
+          <polygon
+            stroke="#a3a3a3" //border
+            fill="#fff" //background
+            // onClick={() => handleClick("abajo")}
+            className="cursor-pointer "
+            points="21.445680618286133,25.29296439886093 28.199169158935547,33.092128217220306 0.7631232142448425,33.092128217220306 7.516610622406006,25.29296439886093 "
+          />
+
+          <polygon
+            stroke="#a3a3a3" //border
+            fill="#fff" //background
+            // onClick={() => handleClick("izquierda")}
+            points="0.7631232291460037,1.3051201105117798 0.7631232291460037,33.232784271240234 7.516610696911812,25.189937591552734 7.516610696911812,25.189937591552734 7.516610696911812,9.104242324829102 "
+            className="cursor-pointer "
+          />
+          <polygon
+            stroke="#a3a3a3" //border
+            fill="#fff" //background
+            // onClick={() => handleClick("centro")}
+            points="7.516610696911812,9.104242324829102 21.445681169629097,9.104242324829102 21.445681169629097,25.189937591552734 7.516610696911812,25.189937591552734 "
+            className="cursor-pointer "
+          />
+        </svg>
+      </div>
+
+      {isActive && (
+        <MenuTooth
+          width={width}
+          positionMenu={positionMenu}
+          onSelectOption={onSelectOption}
+        />
       )}
     </div>
   );
