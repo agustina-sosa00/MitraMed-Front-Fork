62cf32253f598827def1f047a44fdfe56494a135
nova <novagestionsoftware@gmail.com>
2025-06-26 15:05:18 -0300

    se pintan correctamente las caras de los dientes con restauraciones

--- src/views/dashboardProfessional/Odontogram/Odontogram.tsx
+++ src/views/dashboardProfessional/Odontogram/Odontogram.tsx
@@ -117,7 +117,7 @@ export const Odontogram = () => {
               },
             }))
           }
-          action={teethState[toothSelect]?.action || ""}
+          action={teethState[toothSelect]?.action}
           setAction={(action) =>
             setTeethState((prev) => ({
               ...prev,
