fbab4f939b041c1a59429ce72b2816c69ab1e3ec
nova <novagestionsoftware@gmail.com>
2025-06-27 11:36:19 -0300

    btn de wpp mas chico en mobile y laptop

--- src/components/features/WhatsApp.tsx
+++ src/components/features/WhatsApp.tsx
@@ -9,9 +9,9 @@ export default function WhatsApp() {
         <img
           src="/whatsapp-logo-icon.png"
           alt="WhatsApp"
-          className="object-cover w-16 h-16 sm:w-20 sm:h-20"
+          className="object-cover w-12 h-12 sm:w-14 sm:h-14"
         />
-        <span className="absolute hidden px-2 py-1 mb-2 text-sm rounded bottom-5 right-24 group-hover:block bg-[#f1f1f1] text-[#212121]">
+        <span className="absolute hidden px-2 py-1 mb-2 text-sm rounded bottom-5 right-16 lg:right-20 group-hover:block bg-[#f1f1f1] text-[#212121]">
           Contáctanos
         </span>
       </a>
