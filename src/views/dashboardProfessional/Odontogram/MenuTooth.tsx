0990319863f7614936fc41c6b5fff1bbbb38d206
nova <novagestionsoftware@gmail.com>
2025-07-07 12:04:07 -0300

    boton de limpiar diente

--- src/views/dashboardProfessional/Odontogram/MenuTooth.tsx
+++ src/views/dashboardProfessional/Odontogram/MenuTooth.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { ButtonSubMenu } from "./ButtonSubMenu";
+import { LuEraser } from "react-icons/lu";
+import Swal from "sweetalert2";
 
 interface MenuToothProps {
   positionMenu: { x: number; y: number };
   width: boolean;
   updateTooth: (newData: { action?: string; tratamiento?: string }) => void;
   handle: () => void;
+  tooth: number;
 }
 
 export const MenuTooth: React.FC<MenuToothProps> = ({
@@ -14,10 +17,31 @@ export const MenuTooth: React.FC<MenuToothProps> = ({
   width,
   updateTooth,
   handle,
+  tooth,
 }) => {
   const [openSubMenu, setOpenSubMenu] = useState<
     "realizado" | "a realizar" | null
   >(null);
+
+  const handleCleanTooth = () => {
+    Swal.fire({
+      title: `¿Desea quitar las modificaciones del diente n° ${tooth}?`,
+      text: "Esta acción no se puede deshacer",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#518915",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        updateTooth({
+          action: "",
+          tratamiento: "",
+        });
+      }
+    });
+  };
 
   return (
     <div
@@ -30,7 +54,7 @@ export const MenuTooth: React.FC<MenuToothProps> = ({
         onMouseLeave={() => setOpenSubMenu(null)}
       >
         <div
-          className={`flex items-center justify-between w-full h-full px-3 py-2 text-sm rounded-t cursor-pointer text-blue hover:text-white hover:bg-green ${
+          className={`flex items-center justify-between transition-all duration-300 w-full h-full px-3 py-2 text-sm rounded-t cursor-pointer text-blue hover:text-white hover:bg-green ${
             openSubMenu === "realizado" && "bg-green text-white "
           } `}
         >
@@ -59,7 +83,7 @@ export const MenuTooth: React.FC<MenuToothProps> = ({
         onMouseLeave={() => setOpenSubMenu(null)}
       >
         <div
-          className={`flex items-center justify-between w-full h-full px-3 py-2 text-sm rounded-b cursor-pointer text-blue hover:text-white hover:bg-green ${
+          className={`flex items-center justify-between w-full transition-all duration-300 h-full px-3 py-2 text-sm  cursor-pointer text-blue hover:text-white hover:bg-green ${
             openSubMenu === "a realizar" && "bg-green text-white"
           } `}
         >
@@ -80,6 +104,15 @@ export const MenuTooth: React.FC<MenuToothProps> = ({
             />
           </div>
         )}
+      </div>
+
+      <div className="relative h-1/2" onMouseLeave={() => setOpenSubMenu(null)}>
+        <div
+          className={`flex items-center justify-between w-full h-full px-3 py-2 text-sm rounded-b cursor-pointer text-red-600 transition-all duration-300  font-medium hover:bg-[#dddddd] `}
+          onClick={handleCleanTooth}
+        >
+          Limpiar diente <LuEraser />
+        </div>
       </div>
     </div>
   );
