235139b96dc42c503c2e181befc3ae3a5c9c97a1
nova <novagestionsoftware@gmail.com>
2025-07-09 09:19:55 -0300

    bg-cover en perfil profesional

--- src/layouts/ProfessionalLayout.tsx
+++ src/layouts/ProfessionalLayout.tsx
@@ -42,7 +42,7 @@ export const ProfessionalLayout: React.FC<IProp> = ({ setLoader }) => {
     }, 300);
   }, []);
   return (
-    <div className="flex flex-col w-full min-h-screen bg-right bg-no-repeat lg:flex-row bg-profesional ">
+    <div className="flex flex-col w-full min-h-screen bg-right bg-no-repeat bg-cover lg:flex-row bg-profesional ">
       <Navbar logo={"https://i.imgur.com/HBsiL82.png"} buttons={buttons} />
       <SideBar logo={"https://i.imgur.com/HBsiL82.png"} buttons={buttons} />
       <div className="w-full ">
