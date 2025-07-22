623fa492143daad1aa396e7b92bb621f0bb9259d
nova <novagestionsoftware@gmail.com>
2025-07-22 17:12:33 -0300

    maquetacion de historial junto con la tabla

--- src/views/dashboardProfessional/MedicalHistory.tsx
+++ src/views/dashboardProfessional/MedicalHistory.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { SearchPatient } from "@/components/features/PanelProfessional/SearchPatient";
 import { FormUploadHistory } from "@/components/features/PanelProfessional/FormUploadHistory";
+import { TablaDefault } from "@/frontend-resourses/components";
 
 export const MedicalHistory: React.FC = () => {
   const [infoUser, setInfoUser] = useState<boolean>(false);
@@ -9,7 +10,7 @@ export const MedicalHistory: React.FC = () => {
     setInfoUser(!infoUser);
   };
   return (
-    <div className="flex flex-col w-full min-h-screen px-10 pt-10 ">
+    <div className="flex flex-col w-full min-h-screen px-6 pt-10 ">
       <div className="flex flex-col w-full ">
         <h1 className="text-2xl font-medium uppercase lg:text-4xl text-green">
           Historial médico
@@ -24,8 +25,84 @@ export const MedicalHistory: React.FC = () => {
         />
       </div>
 
-      <div className="flex w-full ">
-        <div className="w-1/2">as</div>
+      <div className="flex w-full gap-2 ">
+        <div className="w-1/2">
+          <TablaDefault
+            props={{
+              datosParaTabla: [
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+                {
+                  id: "1",
+                  fecha: "08/07/2025",
+                  motivo: "Consulta",
+                  profesional: "Jean Pietro Mortarini",
+                },
+              ],
+              objectColumns: [
+                {
+                  key: "id",
+                  label: "id",
+                  minWidth: "40",
+                  maxWidth: "40",
+                },
+                {
+                  key: "fecha",
+                  label: "Fecha",
+                  minWidth: "100",
+                  maxWidth: "100",
+                },
+                {
+                  key: "motivo",
+                  label: "Motivo de consulta",
+                  minWidth: "250",
+                  maxWidth: "250",
+                },
+
+                {
+                  key: "profesional",
+                  label: "Profesional",
+                  minWidth: "160",
+                  maxWidth: "160",
+                },
+              ],
+              objectStyles: {
+                addHeaderColor: "#022539",
+                columnasNumber: [1],
+                containerClass: "border border-gray-300 rounded-t-lg",
+                withBorder: false,
+              },
+            }}
+          />
+        </div>
         <FormUploadHistory />
       </div>
     </div>
