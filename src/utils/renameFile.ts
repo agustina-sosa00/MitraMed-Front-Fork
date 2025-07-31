cb778c998c3d54c2cfbd99b8b8efd4a2ecf2a9db
nova <novagestionsoftware@gmail.com>
2025-07-31 10:28:35 -0300

    rename a los archivos dni+iddoctor+fechayhora

--- src/utils/renameFile.ts
+++ src/utils/renameFile.ts
@@ -1,8 +1,23 @@
-export const renameFile = (archivoOriginal: File | null) => {
+export const renameFile = ({
+  archivoOriginal,
+  idDoctor,
+}: {
+  archivoOriginal: File | null;
+  idDoctor: string;
+}) => {
   if (!archivoOriginal) return null;
 
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
   const extension = archivoOriginal.name.split(".").pop();
-  const nuevoNombre = `archivo_${Date.now()}.${extension}`;
+  const nuevoNombre = `42373284_${idDoctor}_${year}${month}${day}${hours}${minutes}${seconds}.${extension}`;
 
   return new File([archivoOriginal], nuevoNombre, {
     type: archivoOriginal.type,
