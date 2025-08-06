699b1ef49e700722d5d8b7a5d52af8cc290ff6aa
nova <novagestionsoftware@gmail.com>
2025-08-06 17:08:14 -0300

    vista de metricas

--- src/views/dashboardProfessional/Metrics.tsx
+++ src/views/dashboardProfessional/Metrics.tsx
@@ -1,27 +1,35 @@
-import DonutChartWithLabels from "@/components/features/PanelProfessional/DonutChart";
+import { ContainView } from "@/components/features/PanelProfessional/ContainView";
 import React from "react";
+import { DetailsMetrics } from "./DetailsMetrics";
 
-//#region Tooltip
+//#region arrays
 const metodoDePago = [
-  { name: "mp", value: 400 },
-  { name: "ef", value: 300 },
-  { name: "cred", value: 200 },
-  { name: "deb", value: 100 },
+  { name: "efectivo", value: 400, colors: "#00911F" },
+  { name: "mercado pago", value: 300, colors: "#00B5F6" },
+  { name: "débito", value: 100, colors: "#0050BF" },
+  { name: "crédito", value: 200, colors: "#EB9C09" },
 ];
 const obs = [
-  { name: "Swiss Medical", value: 100 },
-  { name: "IOMA", value: 300 },
-  { name: "S.S", value: 200 },
+  { name: "Swiss Medical", value: 100, colors: "#D50032" },
+  { name: "IOMA", value: 300, colors: "#04AAC0" },
+  { name: "Osde", value: 300, colors: "#1226AA" },
+  { name: "Medifé", value: 210, colors: "#f37d2e" },
+];
+const turns = [
+  { name: "clínica medica", value: 110, colors: "#1A9AE9" },
+  { name: "odontología", value: 310, colors: "#00BF89" },
+  { name: "Cardiología", value: 500, colors: "#D50032" },
+  { name: "Pediatría", value: 90, colors: "#ECDC2C" },
 ];
 
 export const Metrics: React.FC = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div>
-        <DonutChartWithLabels data={metodoDePago} />{" "}
+    <ContainView title="métricas generales">
+      <div className="grid w-full grid-cols-2 gap-5 justify-items-center">
+        <DetailsMetrics data={metodoDePago} title="método de pago" />
+        <DetailsMetrics data={obs} title="obras sociales" />
+        <DetailsMetrics data={turns} title="turnos por especialidad" />
       </div>
-
-      <DonutChartWithLabels data={obs} />
-    </div>
+    </ContainView>
   );
 };
