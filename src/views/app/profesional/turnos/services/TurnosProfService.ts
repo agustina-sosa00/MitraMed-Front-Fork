b3d291ab4a241737fc67b4107130a32cf0ebc1aa
novagestionsoftware <novagestionsoftware@gmail.com>
2025-09-16 10:22:06 -0300

    fix service reading localstorage _iddoc

--- src/views/app/profesional/turnos/services/TurnosProfService.ts
+++ src/views/app/profesional/turnos/services/TurnosProfService.ts
@@ -5,19 +5,13 @@ export async function obtenerTurnosDiarios({ fini, ffin }) {
     const empresa = localStorage.getItem("_e");
     const modo = localStorage.getItem("_m");
     const tusuario = localStorage.getItem("_tu");
-    const iddoctor = localStorage.getItem("_id");
+    const iddoctor = localStorage.getItem("_iddoc");
 
     const url = `/apinovades/mitramed/obtenerTurnosDiarios.php?_i={"_e":"${empresa}","_m":"${modo}","_tu":"${tusuario}","_id":"${iddoctor}","_fini":"${fini}","_ffin":"${ffin}"}`;
 
     const response = await apiPhp(url);
 
-    let raw = response.data;
-    if (typeof raw === "string") {
-      const idx = raw.indexOf("{");
-      const cortado = idx !== -1 ? raw.slice(idx) : raw;
-      // console.log(cortado);
-      return JSON.parse(cortado);
-    }
+    return response.data;
   } catch (error) {
     throw new Error(`${error}`);
   }
