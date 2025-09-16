529a10b2037f1dfc546af27b5910b3342d8f71b4
novagestionsoftware <novagestionsoftware@gmail.com>
2025-09-16 19:55:53 -0300

    agrego funciton getToday

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,11 @@ export const formatDate = (date: Date) => {
 
   return `${year}-${month}-${day}`;
 };
+
+export function getToday() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
