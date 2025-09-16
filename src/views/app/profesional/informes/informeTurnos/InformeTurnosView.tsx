11711e950ab376c36b8e404596a29ef81f3740ee
novagestionsoftware <novagestionsoftware@gmail.com>
2025-09-16 16:04:19 -0300

    fix nombre card

--- src/views/app/profesional/informes/informeTurnos/InformeTurnosView.tsx
+++ src/views/app/profesional/informes/informeTurnos/InformeTurnosView.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { ContainView } from "@/views/app/components/features/ContainView";
 import TableCard from "./components/TableCard";
 import { useInformeTurnosStore } from "./store/informeTurnosStore";
-import DateRangeCard from "@/views/app/profesional/informes/informeTurnos/components/DateRangeCard";
 import MetricasCard from "./components/MetricasCard";
+import HeaderCard from "./components/HeaderCard";
 
 export default function InformeTurnosView() {
   const { informeTurnosData, setHasSearched } = useInformeTurnosStore();
@@ -19,7 +19,7 @@ export default function InformeTurnosView() {
   return (
     <ContainView title="informe de turnos" padding="mt-2 px-4" classContainer="">
       {/* Buscador */}
-      <DateRangeCard loader={loader} setLoader={setLoader} />
+      <HeaderCard loader={loader} setLoader={setLoader} />
 
       {/* Tabla */}
       <TableCard />
