d7d64039542901092b0ee159d8d5ec9555f1286b
novagestionsoftware <novagestionsoftware@gmail.com>
2025-09-24 19:49:01 -0300

    comento api dropbox

--- src/lib/axiosDropbox.ts
+++ src/lib/axiosDropbox.ts
@@ -1,41 +1,41 @@
-import { getAccessTokenDropbox } from "@/views/app/profesional/hc/service/HistorialClinicoService";
-import axios from "axios";
-import Cookies from "js-cookie";
+// import { getAccessTokenDropbox } from "@/views/app/profesional/hc/service/HistorialClinicoService";
+// import axios from "axios";
+// import Cookies from "js-cookie";
 
-export const apiDropbox = axios.create({
-  baseURL: "https://content.dropboxapi.com",
-});
+// export const apiDropbox = axios.create({
+//   baseURL: "https://content.dropboxapi.com",
+// });
 
-apiDropbox.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const status = error?.response?.status;
-    const tag = error?.response?.data?.error?.[".tag"];
-    const isExpiredToken = status === 401 && tag === "expired_access_token";
-    if (isExpiredToken) {
-      try {
-        const refreshToken = Cookies.get("refreshTokenDropbox") || "";
-        const app_id = Cookies.get("appIdDropbox") || "";
-        const app_secret = Cookies.get("appSecretDropbox") || "";
-        const newTokenAccessData = await getAccessTokenDropbox({
-          refreshToken: refreshToken,
-          clientId: app_id,
-          clientSecret: app_secret,
-        });
-        const newTokenAccessDropbox = newTokenAccessData.access_token;
-        Cookies.set("accessTokenDropbox", newTokenAccessDropbox, {
-          expires: 5 / 24,
-        });
+// apiDropbox.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const status = error?.response?.status;
+//     const tag = error?.response?.data?.error?.[".tag"];
+//     const isExpiredToken = status === 401 && tag === "expired_access_token";
+//     if (isExpiredToken) {
+//       try {
+//         const refreshToken = Cookies.get("refreshTokenDropbox") || "";
+//         const app_id = Cookies.get("appIdDropbox") || "";
+//         const app_secret = Cookies.get("appSecretDropbox") || "";
+//         const newTokenAccessData = await getAccessTokenDropbox({
+//           refreshToken: refreshToken,
+//           clientId: app_id,
+//           clientSecret: app_secret,
+//         });
+//         const newTokenAccessDropbox = newTokenAccessData.access_token;
+//         Cookies.set("accessTokenDropbox", newTokenAccessDropbox, {
+//           expires: 5 / 24,
+//         });
 
-        error.config.headers["Authorization"] = `Bearer ${newTokenAccessDropbox}`;
-        return apiDropbox.request(error.config);
-      } catch (e) {
-        return Promise.reject(e);
-      }
-    }
+//         error.config.headers["Authorization"] = `Bearer ${newTokenAccessDropbox}`;
+//         return apiDropbox.request(error.config);
+//       } catch (e) {
+//         return Promise.reject(e);
+//       }
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
