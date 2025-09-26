8a6376802bd917084cf90f59e16e8bbe53f53e4f
novagestionsoftware <novagestionsoftware@gmail.com>
2025-09-26 10:59:38 -0300

    arreglo losplaceholder

--- src/router.tsx
+++ src/router.tsx
@@ -26,6 +26,7 @@ import UsuariosProfesionalesView from "./views/app/profesional/usuarios/Usuarios
 import ConfiguracionView from "./views/app/profesional/configuracion/ConfiguracionView";
 import EnvioEmailPacView from "./views/app/profesional/procesos/email/emailPac/EnvioEmailPacView";
 import EnvioEmailProfView from "./views/app/profesional/procesos/email/emailProf/EnvioEmailProfView";
+import PlaceHolderDesarrolloView from "./views/app/profesional/placeholderDesarrollo/PlaceholderDesarrolloView";
 
 interface RouterProps {
   loader: boolean;
@@ -33,14 +34,14 @@ interface RouterProps {
 }
 
 export default function Router({ loader, setLoader }: RouterProps) {
-  // Para control inline de /dashboard/turnos-profesional
   const [env, setEnv] = useState<string | null>(null);
+  // const isDevelopment = import.meta.env.VITE_ENV === "development";
+
   useEffect(() => {
     if (typeof window !== "undefined") {
       setEnv(localStorage.getItem("_env"));
     }
   }, []);
-  // const idProfesional = Cookies.get("idProfesional");
 
   return (
     <BrowserRouter>
@@ -49,8 +50,8 @@ export default function Router({ loader, setLoader }: RouterProps) {
         <Route path="/" element={<PortalView loader={loader} setLoader={setLoader} />} index />
         <Route path="/privacy-policy" element={<PoliticasDePrivacidadView />} />
         <Route path="/terms-of-service" element={<TerminosYCondicionesView />} />
+        <Route path="*" element={<Vista404 />} />
 
-        {/* Rutas protegidas */}
         <Route element={<PacienteProtectedRoute />}>
           <Route element={<PacienteLayout setLoader={setLoader} />}>
             <Route path="/inicio" element={<HomePacientesView />} index />
@@ -60,30 +61,25 @@ export default function Router({ loader, setLoader }: RouterProps) {
           </Route>
         </Route>
 
-        {
-          //region dashboard profesional
-        }
         <Route element={<ProfessionalProtectedRoute />}>
           <Route element={<ProfessionalLayout setLoader={setLoader} />}>
             <Route path="/dashboard/inicio" element={<HomeProfesionalView />} />
-            <Route
-              path="/dashboard/turnos"
-              element={env === "des" ? <TurnosGeneralesView /> : <Vista404 />}
-            />
+            <Route path="/dashboard/turnos" element={<TurnosGeneralesView />} />
             <Route path="/dashboard/turnos-profesional" element={<TurnosProfesionalView />} />
             <Route path="/dashboard/historia-clinica" element={<HistorialClinicoView />} />
             <Route path="/dashboard/odontograma" element={<OdontogramView />} />
             <Route path="/dashboard/informe-turnos" element={<InformeTurnosView />} />
             <Route path="/dashboard/procesos/envio-email-prof" element={<EnvioEmailProfView />} />
-            <Route path="/dashboard/procesos/envio-email-pac" element={<EnvioEmailPacView />} />
+            <Route
+              path="/dashboard/procesos/envio-email-pac"
+              element={env === "des" ? <EnvioEmailPacView /> : <PlaceHolderDesarrolloView />}
+            />
             <Route path="/dashboard/usuarios" element={<UsuariosProfesionalesView />} />
             <Route path="/dashboard/configuracion" element={<ConfiguracionView />} />
             {/* {idProfesional && (
             )} */}
           </Route>
         </Route>
-
-        <Route path="*" element={<Vista404 />} />
       </Routes>
     </BrowserRouter>
   );
